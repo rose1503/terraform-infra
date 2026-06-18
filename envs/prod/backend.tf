338a3cda23ce488f9302389a32c6c15f0527d412
Rose <kamdoum.rosine@yahoo.fr>
2026-06-17 21:16:30 -0400

    feat(prod): did some changes in the code

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraformweek2026"
+    bucket       = "bucket-name"
     key          = "envs/prod/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
